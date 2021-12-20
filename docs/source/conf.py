b8176000554fc89e4a0e3c4d3b79b2526b6cf050
Don-King-Kong <74015261+Don-King-Kong@users.noreply.github.com>
2021-12-20 15:28:00 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,10 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import stanford_theme
     html_theme = 'stanford_theme'
-    html_theme_path = [stanford_theme.get_html_theme_path()]
+    html_theme_path = ['_themes/stanford_theme']
+    html_sidebars = []
+    
+    #html_theme_path = [stanford_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 

e4001ceba32ad6f0761b572d90b10a634d8e0af4
Don-King-Kong <74015261+Don-King-Kong@users.noreply.github.com>
2021-12-20 17:06:14 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ exclude_patterns = []
 # -- Options for HTML output
 
 def setup(app):
-    app.add_css_file('custom.css')
+    app.add_css_file('_static/custom.css')
 
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 #import os, sys
@@ -50,6 +50,23 @@ def setup(app):
 
 
 html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 
 # -- Options for EPUB output
